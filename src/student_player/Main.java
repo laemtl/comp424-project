de5cac3b3deb5f236bb814ae5196d45aa7c922eb
Nicolas Blackburn <nicolas@nblackburn.ca>
2019-04-03 21:57:48 -0400

    Reformat main and node classes

--- src/student_player/Main.java
+++ src/student_player/Main.java
@@ -13,8 +13,7 @@ import pentago_swap.PentagoMove;
 import pentago_swap.PentagoBoardState.Quadrant;
 
 public class Main {
-	
-	
+
 	public static void main(String[] args) {
 		// TL, TR, BL, BR
 		// Study Case
@@ -24,33 +23,24 @@ public class Main {
 		int br = 3;
 		int w = 0;
 		int b = 1;
-		
-		PentagoBoardState state1 = BoardFactory.createState(new int[][]{
-			{1, 1, tl, tr},
-			{0, 0, tl, tr},
-			{0, 0, tl, tr},
-			{2, 5, tl, br},
-			{0, 4, tl, tr},
-			{1, 2, tl, tr},
-			{0, 5, tl, tr},
-			{0, 4, tl, tr},
-		});
-		PentagoBoardState state2 = (PentagoBoardState)state1.clone();
 
-		state1.processMove(BoardFactory.createMove(new int[] {2, 4, tl, tr}, w));
-		state2.processMove(BoardFactory.createMove(new int[] {3, 1, tl, bl}, w));
+		PentagoBoardState state1 = BoardFactory
+				.createState(new int[][] { { 1, 1, tl, tr }, { 0, 0, tl, tr }, { 0, 0, tl, tr }, { 2, 5, tl, br },
+						{ 0, 4, tl, tr }, { 1, 2, tl, tr }, { 0, 5, tl, tr }, { 0, 4, tl, tr }, });
+		PentagoBoardState state2 = (PentagoBoardState) state1.clone();
+
+		state1.processMove(BoardFactory.createMove(new int[] { 2, 4, tl, tr }, w));
+		state2.processMove(BoardFactory.createMove(new int[] { 3, 1, tl, bl }, w));
 
 		System.out.println(String.format("Score: %d", HeuristicFunction.compute(0, state1)));
-	    System.out.println(state1);
+		System.out.println(state1);
 
 		System.out.println(String.format("Score: %d", HeuristicFunction.compute(0, state2)));
-	    System.out.println(state2);
-	    
-	    Node<Integer> node = Node.expand(
-	    		n -> new Pair<>(n, Arrays.asList(n + 1, n + 2, n + 3)), 
-	    		0);
-	    
-	    String result = node.map(x -> x * x).truncateAtDepth(3).toString();
-	    System.out.println(result);
+		System.out.println(state2);
+
+		Node<Integer> node = Node.expand(n -> new Pair<>(n, Arrays.asList(n + 1, n + 2, n + 3)), 0);
+
+		String result = node.map(x -> x * x).truncateAtDepth(3).toString();
+		System.out.println(result);
 	}
 }
--- src/student_player/Node.java
+++ src/student_player/Node.java
@@ -7,82 +7,72 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
-class Pair<A, B> {
-	public A left;
-	public B right;
-	Pair(A left, B right) {
-		this.left = left;
-		this.right = right;
-	}
-}
-
 /**
  * A generic node class to represent tree data structures.
  *
  * @param <A>
  */
 public class Node<A> {
+	public static <A, B> Node<B> expand(Function<A, Pair<B, List<A>>> func, A a) {
+		Pair<B, List<A>> pair = func.apply(a);
+		Supplier<List<Node<B>>> children = () -> pair.right.stream().map((a2) -> Node.expand(func, a2))
+				.collect(Collectors.toList());
+		return new Node<B>(pair.left, children);
+	}
+	
 	private A value;
-    private List<Node<A>> children;
-    private Supplier<List<Node<A>>> childrenSupplier;
-    
-    public Node(A value, Supplier<List<Node<A>>> childrenSupplier) {
-    	this.value = value;
-    	this.childrenSupplier = childrenSupplier;
-    }
-    
-    public A getValue() {
-    	return value;
-    }
-    
-    public List<Node<A>> getChildren() {
-    	if (children == null) {
-    		children = childrenSupplier.get();
-    	}
-    	return children;
-    }
-    
-    public <B> Node<B> map(Function<A, B> func) {
-    	return new Node<B>(func.apply(value), () -> getChildren()
-    			.stream()
-    			.map(node -> node.map(func))
-    			.collect(Collectors.toList()));
-    }
+	private List<Node<A>> children;
+	private Supplier<List<Node<A>>> childrenSupplier;
 
-    public <B> B reduce(BiFunction<A, List<B>, B> func) {
-    	return func.apply(value, getChildren()
-    			.stream()
-    			.map(node -> node.reduce(func))
-    			.collect(Collectors.toList()));
-    }
+	public Node(A value, Supplier<List<Node<A>>> childrenSupplier) {
+		this.value = value;
+		this.childrenSupplier = childrenSupplier;
+	}
 
-    public static <A, B> Node<B> expand(Function<A, Pair<B, List<A>>> func, A a) {
-    	Pair<B, List<A>> pair = func.apply(a);
-    	Supplier<List<Node<B>>> children = () -> pair.right
-    			.stream()
-    			.map((a2) -> Node.expand(func, a2))
-    			.collect(Collectors.toList());
-    	return new Node<B>(pair.left, children);
-    }
-    
-    public Node<A> truncateAtDepth(int n) {
-    	Supplier<List<Node<A>>> supplier = () -> n <= 1 ? 
-    			  Arrays.asList()
-    			: getChildren()
-    				.stream()
-    				.map(child -> child.truncateAtDepth(n - 1))
-    				.collect(Collectors.toList());
-    	return new Node<>(value, supplier);
-    }
-    
-    public String toString() {
-    	return this.reduce((value, folds) -> {
-	    	String str = "";
-	    	for (String fold: folds) {
-	    		str += (str.length() == 0 ? "" : ", ") + fold;
-	    	}
-	    	str = str.length() != 0 ? String.format(", children: {%s}", str) : str;
-	    	return String.format("Node(%s%s)", value.toString(), str);
-	    });
-    }
+	public List<Node<A>> getChildren() {
+		if (children == null) {
+			children = childrenSupplier.get();
+		}
+		return children;
+	}
+
+	public A getValue() {
+		return value;
+	}
+
+	public <B> Node<B> map(Function<A, B> func) {
+		return new Node<B>(func.apply(value),
+				() -> getChildren().stream().map(node -> node.map(func)).collect(Collectors.toList()));
+	}
+
+	public <B> B reduce(BiFunction<A, List<B>, B> func) {
+		return func.apply(value, getChildren().stream().map(node -> node.reduce(func)).collect(Collectors.toList()));
+	}
+
+	public String toString() {
+		return this.reduce((value, folds) -> {
+			String str = "";
+			for (String fold : folds) {
+				str += (str.length() == 0 ? "" : ", ") + fold;
+			}
+			str = str.length() != 0 ? String.format(", children: {%s}", str) : str;
+			return String.format("Node(%s%s)", value.toString(), str);
+		});
+	}
+
+	public Node<A> truncateAtDepth(int n) {
+		Supplier<List<Node<A>>> supplier = () -> n <= 1 ? Arrays.asList()
+				: getChildren().stream().map(child -> child.truncateAtDepth(n - 1)).collect(Collectors.toList());
+		return new Node<>(value, supplier);
+	}
+}
+
+class Pair<A, B> {
+	public A left;
+	public B right;
+
+	Pair(A left, B right) {
+		this.left = left;
+		this.right = right;
+	}
 }
